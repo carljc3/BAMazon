b5c141f1a5f9a7dc61e1c6ce5d1e9ca9f2e4e1aa
J-Anne Carlson <carlsonjc3@J-Annes-MacBook-Pro.local>
2019-11-06 10:48:29 -0800

    updated schema

--- schema.sql
+++ schema.sql
@@ -1,10 +1,16 @@
+DROP DATABASE IF EXISTS BAMazonDB;
+CREATE DATABASE BAMazonDB;
+USE BAMazonDB;
 
+
+CREATE TABLE PRODUCTS (
 item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
 product_name VARCHAR(100),
 department_name VARCHAR(100),
-price INTEGER(11) AUTO_INCREMENT NOT NULL, <--- Decimal
+price INTEGER(11) AUTO_INCREMENT NOT NULL,
 stock_quantity INTEGER(11) AUTO_INCREMENT NOT NULL,
 primary key(id)
+);
 
 INSERT INTO PRODUCTS (product_name, department_name, price, stock_quanity)
 VALUES ("Galaxy Yoga Pants", "Lululemon", "98.00", "5");
@@ -19,7 +25,7 @@ INSERT INTO PRODUCTS (product_name, department_name, price, stock_quanity)
 VALUES ("Pink Yoga Mat", "Lululemon", "36.00", "10");
 
 INSERT INTO PRODUCTS (product_name, department_name, price, stock_quanity)
-VALUES ("Blue Yoga Mat", "Lululemon", "36.00", "10");
+VALUES ("Blue Yoga Mat", "Lululemon", "98.00", "10");
 
 INSERT INTO PRODUCTS (product_name, department_name, price, stock_quanity)
 VALUES ("Elastic Non-slip Headbands", "Lululemon", "15.00", "12");
@@ -31,7 +37,7 @@ INSERT INTO PRODUCTS (product_name, department_name, price, stock_quanity)
 VALUES ("Galaxy Yoga Bra", "Lululemon", "40.00", "6");
 
 INSERT INTO PRODUCTS (product_name, department_name, price, stock_quanity)
-VALUES ("Black Yoga Bra", "Lululemon", "40.00", "8");
+VALUES ("Black Yoga Bra", "Lululemon", "20.00", "8");
 
 INSERT INTO PRODUCTS (product_name, department_name, price, stock_quanity)
 VALUES ("Yoga Towel", "Lululemon", "10.00", "18");
